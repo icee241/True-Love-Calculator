2a5903dfd5a1a0f2d7026a76e768b1cb03850024
ice <155775481+icee241@users.noreply.github.com>
2024-02-20 18:41:58 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import random
+print("Welcome to the Real Love Calculator!")
+name1=input("First person:")
+
+name2=input("Second Person:")
+
+percentage=random.randint(10,100)
+
+print("The percentage for" + " " + name1 + " " + name2 + " " + "is" + " " + str(percentage) + "%" + "!")
+
